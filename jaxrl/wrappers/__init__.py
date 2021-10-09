b067265aed0746a53b3235fa9c32521d0bba1661
Erwin Coumans <erwin.coumans@gmail.com>
2021-10-08 21:32:18 -0700

    remove dm_control deps

--- jaxrl/wrappers/__init__.py
+++ jaxrl/wrappers/__init__.py
@@ -1,5 +1,4 @@
 from jaxrl.wrappers.absorbing_states import AbsorbingStatesWrapper
-from jaxrl.wrappers.dmc_env import DMCEnv
 from jaxrl.wrappers.episode_monitor import EpisodeMonitor
 from jaxrl.wrappers.frame_stack import FrameStack
 from jaxrl.wrappers.repeat_action import RepeatAction
